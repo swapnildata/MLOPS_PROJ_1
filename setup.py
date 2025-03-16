c5ab39779e449b8e42720bdea36a55aafc0dc34c
swapnil <swapnilpowar8251@gmail.com>
2025-03-16 17:24:37 +0530

    proj struxture ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Swapnil Powar",
+    author_email="swapnilpowar8251@gmail.com",
+    packages=find_packages()
+)
